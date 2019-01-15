8145646ec855b90e47ec9ef7e66f5a9c7c8dd732
TheGreatCookieMachine <39784551+TheGreatCookieMachine@users.noreply.github.com>
2019-01-14 23:47:53 -0800

    A Little More Work on GUI
    Board.py
    - Added takeInput(), the basis for taking user input
    - Modified self.moveLogLabel to wrap when text is too long

--- GUI/Board.py
+++ GUI/Board.py
@@ -2,14 +2,17 @@ import tkinter
 
 class Board():
     def __init__(self):
+        self.moves = tkinter.StringVar()
+        
         self.boardImage = tkinter.PhotoImage(file="ChessBoard.gif")
         self.boardLabel = tkinter.Label(image=self.boardImage, borderwidth=1, relief="solid")
 
         self.moveLogLabelFrame = tkinter.LabelFrame(height=325, width=175, borderwidth=1, relief="solid")
-        self.moveLogLabel = tkinter.Label(self.moveLogLabelFrame, text="White Move Black Move\nWhite Move Black Move", font=("Calbri", 11), anchor="nw")
+        self.moveLogLabel = tkinter.Label(self.moveLogLabelFrame, textvariable=self.moves, font=("Calbri", 11), anchor="nw", justify="left", wraplength=175)
 
         self.inputLabelFrame = tkinter.LabelFrame(height=25, width=175, borderwidth=1, relief="solid")
         self.inputLabel = tkinter.Entry(self.inputLabelFrame, font=("Calbri", 12), width=18)
+        self.inputLabel.bind("<Return>", self.takeInput)
 
         self.errorLogLabelFrame = tkinter.LabelFrame(height=25, width=175, borderwidth=1, relief="solid")
         self.errorLogLabel = tkinter.Label(self.errorLogLabelFrame, text="Error: Error Example", font=("Calbri", 11), anchor="nw")
@@ -25,3 +28,8 @@ class Board():
 
         self.errorLogLabelFrame.place(x=450, y=400)
         self.errorLogLabel.place(x=0, y=0)
+    
+    def takeInput(self, event):
+        userInput = self.inputLabel.get()
+        self.inputLabel.delete(0, "end")
+        self.moves.set(self.moves.get() + userInput + "\n")
