0be6e34e3a30ae432a4dd5f27385f08580966388
TheGreatCookieMachine <39784551+TheGreatCookieMachine@users.noreply.github.com>
2019-01-19 00:19:08 -0800

    Piece Transparency Now Works
    Board.py
    - Moved from a label to a canvas for the board allowing for transparency

--- GUI/Board.py
+++ GUI/Board.py
@@ -6,11 +6,12 @@ class Board():
         self.moves = tkinter.StringVar()
         self.error = tkinter.StringVar()
         self.pieces = []
-        self.numberSwap = [7, 6, 5, 4, 3, 2, 1, 0] # Used in printScreen to ensure the board is printed the right way
+        self.numberSwap = (7, 6, 5, 4, 3, 2, 1, 0) # Used in printScreen to ensure the board is printed the right way
         
-        self.boardLabelImage = tkinter.PhotoImage(file="GUI\\Assests\\ChessBoard.gif")
-        self.boardLabel = tkinter.Label(image=self.boardLabelImage, borderwidth=1, relief="solid")
-
+        self.boardCanvas = tkinter.Canvas(height=400, width=400, borderwidth=1, relief="solid")
+        self.boardImage = tkinter.PhotoImage(file="GUI\\Assests\\ChessBoard.gif")
+        self.boardCanvas.create_image(0, 0, image=self.boardImage, anchor="nw")
+        
         self.moveLogLabelFrame = tkinter.LabelFrame(height=325, width=175, borderwidth=1, relief="solid")
         self.moveLogLabel = tkinter.Label(self.moveLogLabelFrame, textvariable=self.moves, font=("Calbri", 11), anchor="nw", justify="left", wraplength=175)
 
@@ -24,7 +25,7 @@ class Board():
         self.session = Game.Game()
     
     def printScreen(self):
-        self.boardLabel.place(x=24, y=24)
+        self.boardCanvas.place(x=24, y=24)
 
         self.moveLogLabelFrame.place(x=450, y=24)
         self.moveLogLabel.place(x=0, y=0)
@@ -38,18 +39,16 @@ class Board():
         self.updateBoard()
 
     def updateBoard(self):
-        for label in self.pieces:
-            label.destroy()
+        self.boardCanvas.delete("all")
+        self.boardCanvas.create_image(3, 3, image=self.boardImage, anchor="nw")
         self.pieces = []
         for column in range(len(self.session.board)):
             for row in range(len(self.session.board[column])):
                 if self.session.board[column][row] != " ":
                     piece = self.session.board[column][row]
                     image = tkinter.PhotoImage(file="GUI\\Assests\\" + str(piece.side.capitalize()) + str(piece.title) + ".gif")
-                    label = tkinter.Label(self.boardLabel, image=image)
-                    label.image = image
-                    self.pieces.append(label)
-                    self.pieces[-1].place(x=row * 50, y=self.numberSwap[column] * 50)
+                    self.pieces.append(image)
+                    self.boardCanvas.create_image(row * 50 + 3, self.numberSwap[column] * 50 + 3, image=self.pieces[-1], anchor="nw")
     
     def takeInput(self, event):
         userInput = self.inputLabel.get()
